61a580a6ef6a76305307ca1fc0aeadd1e8ea8db8
Jayesh Puri <84006107+Jayesh-Dev21@users.noreply.github.com>
2025-03-27 01:43:21 +0530

    Add files via upload

---
+++ public/static/js/faceScript.js
@@ -0,0 +1,42 @@
+async function setupCamera() {
+    const video = document.getElementById("video");
+    const stream = await navigator.mediaDevices.getUserMedia({ video: {} });
+    video.srcObject = stream;
+    return new Promise(resolve => video.onloadedmetadata = resolve);
+}
+
+async function loadModels() {
+    await faceapi.nets.tinyFaceDetector.loadFromUri("../../../models");
+    console.log("Model Loaded!");
+}
+
+async function startFaceDetection() {
+    const video = document.getElementById("video");
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+
+    function resizeCanvas() {
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+    }
+
+    resizeCanvas();
+    window.addEventListener("resize", resizeCanvas);
+
+    async function detect() {
+        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions());
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.strokeStyle = "red";
+        ctx.lineWidth = 3;
+
+        detections.forEach(({ box }) => {
+            ctx.strokeRect(box.x, box.y, box.width, box.height);
+        });
+
+        requestAnimationFrame(detect);
+    }
+    detect();
+}
+
+setupCamera().then(loadModels).then(startFaceDetection);
